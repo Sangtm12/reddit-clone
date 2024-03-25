9d0f816a466e351176da4b8dc9bddd24cd8006fc
Sangtm12 <105406591+Sangtm12@users.noreply.github.com>
2024-03-26 01:44:36 +0800

    testing merge conflict

--- src/API/login.js
+++ src/API/login.js
@@ -4,6 +4,7 @@ export async function login({ username, password }) {
       resolve(`Logged in as ${username} (${password})`);
     }, 2000)
   );
+  console.log("test from login-api");
   const response = await promise;
   console.log("created in branch login-api (FOR REAL)");
   return response;
