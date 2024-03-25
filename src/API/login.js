78d72e9c70f0de666078e2c26cc64edb11e7a993
Sangtm12 <105406591+Sangtm12@users.noreply.github.com>
2024-03-26 01:49:52 +0800

    revert back to normal (before testing merge)

--- src/API/login.js
+++ src/API/login.js
@@ -1,13 +1,9 @@
 export async function login({ username, password }) {
-  console.log("test");
   const promise = new Promise((resolve) =>
     setTimeout(() => {
       resolve(`Logged in as ${username} (${password})`);
     }, 2000)
   );
-  console.log("test from login-api");
   const response = await promise;
-  console.log("changed in branch main");
-  console.log("created in branch login-api (FOR REAL)");
   return response;
 }
