fa7fee9dec71e958dec8921dd8b4fd4654454d07
Sangtm12 <105406591+Sangtm12@users.noreply.github.com>
2024-03-31 12:06:25 +0800

    finished login feature

--- src/features/auth/useLogin.js
+++ src/features/auth/useLogin.js
@@ -1,13 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { login as loginApi } from "../../API/login";
 
 export default function useLogin() {
+  const queryClient = useQueryClient();
   const { status, mutate } = useMutation({
     mutationFn: loginApi,
     onSuccess: (data) => {
       toast.success("Logged in successfully");
       window.localStorage.setItem("tokens", JSON.stringify(data));
+      queryClient.invalidateQueries({
+        queryKey: ["user"],
+      });
     },
     onError: (err) => {
       toast.error(err.response.data.error);
