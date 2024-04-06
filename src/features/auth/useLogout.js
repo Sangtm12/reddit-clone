fe71fe0b3311fcd84b66555199673b10f11ce7bf
Sangtm12 <105406591+Sangtm12@users.noreply.github.com>
2024-04-06 12:53:27 +0800

    fixed logoutAPI

--- src/features/auth/useLogout.js
+++ src/features/auth/useLogout.js
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import logOutApi from "../../API/logOut";
+import logoutApi from "../../API/logout";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
@@ -7,7 +7,7 @@ export default function useLogout() {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { status, mutate: logout } = useMutation({
-    mutationFn: logOutApi,
+    mutationFn: logoutApi,
     onSuccess: () => {
       toast.success("Logged out successfully");
       localStorage.removeItem("tokens");
