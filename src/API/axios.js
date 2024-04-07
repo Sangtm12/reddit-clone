98e9a031191ba13700cb53369725c300e6cd900a
Sangtm12 <105406591+Sangtm12@users.noreply.github.com>
2024-04-07 11:51:58 +0800

    deployed server

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://reddit-clone-server.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
